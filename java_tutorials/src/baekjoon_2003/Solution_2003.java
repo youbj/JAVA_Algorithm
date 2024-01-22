42d445ca871640fbe113dc6d3aafd6b50c76c650
ByeongJu <dbqudwn011@naver.com>
2024-01-22 19:41:21 +0900

    +baekjoon_2003

---
+++ java_tutorials/src/baekjoon_2003/Solution_2003.java
@@ -0,0 +1,59 @@
+package baekjoon_2003;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution_2003 {
+
+	public static int count=0;
+
+	public static int m;
+
+	
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st=new StringTokenizer(br.readLine());
+
+		int n;
+
+		n=Integer.parseInt(st.nextToken());
+
+		m=Integer.parseInt(st.nextToken());
+
+		st=new StringTokenizer(br.readLine());
+
+		
+
+		int[] arr=new int[n];
+
+	
+
+		for(int i=0;i<n;i++) {
+
+			arr[i]=Integer.parseInt(st.nextToken());			
+
+		}
+
+		for(int i=0;i<n;i++) {
+			int sum=0;
+			for(int j=i;j<n;j++) {
+				sum+=arr[j];
+				if(sum==m)
+					count++;
+				else if(sum>m)
+					break;
+			}
+		}
+
+				
+
+		System.out.println(count);
+
+	}
+
+
+
+}
