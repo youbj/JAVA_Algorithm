c35d173cf22e850c87e273d43bd26a0579024572
ByeongJu <39815497+youbj@users.noreply.github.com>
2024-02-02 15:07:46 +0900

    [Silver IV] Title: 카드2, Time: 284 ms, Memory: 26032 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,27 +1,44 @@
 import java.util.*;
+/**
+ * @author	유병주 
+ * @date 	02.02
+ * @link	https://www.acmicpc.net/problem/2164
+ * @keyword_solution  queue를 사용하여 출력
+ * @input int형 정수가 들어가기 때문에 신경써야할 부분이 없다.
+ * @output   queue를 사용하여 마지막남은 값 출력
+ * @time_complex  O(N)
+ * @perf 
+ */
 
 public class Main {
-	static Deque<Integer> que=new ArrayDeque<>();
+	static Queue<Integer> que=new ArrayDeque<>();
 	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
 		
 		for(int i=1;i<=n;i++) {
-			que.add(i);
+			if(i%2==0)
+				que.add(i);
 		}
 		
-		while(que.size()!=1) {
-			make_care();
+		if(n%2!=0) {
+			while(que.size()!=1) {
+				que.add(que.poll());
+				que.poll();
+			}
+		}
+		else {
+			while(que.size()!=1) {
+				que.poll();
+				que.add(que.poll());
+			}
 		}
 		System.out.println(que.poll());
 		sc.close();
-	}
-	
-	public static void make_care() {
-		if(que.size()==1)
-			return;
-		que.poll();
-		que.add(que.poll());
 	}
 }
