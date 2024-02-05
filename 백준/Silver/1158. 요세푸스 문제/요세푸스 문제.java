d6b682cef08fc8e3188084a3371b445b891d2cfd
ByeongJu <39815497+youbj@users.noreply.github.com>
2024-02-05 17:50:03 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 396 ms, Memory: 19616 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,13 +1,24 @@
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+/**
+ * @author 유병주	 
+ * @date  02.05
+ * @link https://www.acmicpc.net/problem/1158
+ * @keyword_solution  Queue를 사용하여 반복
+ * @input 5000이하의 K와 N
+ * @output  값이 도출될 때마다 수정해야 하므로 StringBuilder를 사용
+ * @time_complex  O(N^2)
+ * @perf 298328	664
+ */
 
 public class Main {
 	
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder();
 		Scanner sc= new Scanner(System.in);
-		Queue<Integer>que= new LinkedList<Integer>();
+		Queue<Integer>que= new ArrayDeque<Integer>();
 		
 		int n=sc.nextInt();
 		int k= sc.nextInt();
