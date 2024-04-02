02803133af793e6d5375adeb8212bc73baa97ec5
ByeongJu <39815497+youbj@users.noreply.github.com>
2024-04-02 11:36:32 +0900

    [Bronze II] Title: 피보나치 수, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static int [] dp;
+	
+	public static void main(String[] args) throws Exception{
+		int n = Integer.parseInt(br.readLine());
+		
+		dp = new int[n+2];
+		dp[0]=0;
+		dp[1]=1;
+		if(n<=1) {
+			System.out.println(n);
+			return;
+		}
+		
+		for(int i=2;i<=n;i++) {
+			dp[i] = dp[i-2]+dp[i-1];
+		}
+		System.out.println(dp[n]);
+	}
+}
