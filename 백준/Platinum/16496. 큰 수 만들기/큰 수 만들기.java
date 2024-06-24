d72a9082fceae2211cf79f9b4a2059aabc2db069
ByeongJu <39815497+youbj@users.noreply.github.com>
2024-06-24 22:39:41 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 244 ms, Memory: 20896 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.java
@@ -7,37 +7,46 @@ import java.util.StringTokenizer;
 import java.util.Collections;
 
 public class Main {
-    public static String answer;
-    public static StringTokenizer st;
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static String answer;
+	public static StringTokenizer st;
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static List<String> list = new ArrayList<>();
+	public static List<String>[] list = new ArrayList[10];
 
-    public static void main(String[] args) throws Exception {
-        int size = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		int size = Integer.parseInt(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < size; i++) {
-            String value = st.nextToken();
-            list.add(value);
-        }
+		for (int i = 0; i < 10; i++) {
+			list[i] = new ArrayList<String>();
+		}
 
-        Collections.sort(list, (o1, o2) -> {
-            String order1 = o1 + o2;
-            String order2 = o2 + o1;
-            return order2.compareTo(order1);
-        });
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < size; i++) {
+			String value = st.nextToken();
+			int idx = value.charAt(0) - '0';
+			list[idx].add(value);
+		}
 
-        StringBuilder sb = new StringBuilder();
-        for (String num : list) {
-            sb.append(num);
-        }
+		for (int i = 0; i < 10; i++) {
+			list[i].sort((o1, o2) -> {
+				String order1 = o1 + o2;
+				String order2 = o2 + o1;
+				return order2.compareTo(order1);
+			});
+		}
 
-        answer = sb.toString();
-        if (answer.charAt(0) == '0') {
-            answer = "0";
-        }
+		StringBuilder sb = new StringBuilder();
+		for (int i = 9; i >= 0; i--) {
+			for (String value : list[i]) {
+				sb.append(value);
+			}
+		}
 
-        System.out.println(answer);
-    }
+		answer = sb.toString();
+		if (answer.charAt(0) == '0') {
+			answer = "0";
+		}
+
+		System.out.println(answer);
+	}
 }
